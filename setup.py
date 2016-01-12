e5ae795b40a0853284616889a356cc5bea53a73d
Tyler Krupicka <tylerkrupicka@gmail.com>
2016-01-12 15:16:41 -0800

    update package for cc

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
 setup(
-    name='browser-cookie3',
-    version='0.5.0',
-    packages=['browser_cookie3'],
-    package_dir={'browser_cookie3' : '.'}, # look for package contents in current directory
-    author='Boris Babic',
-    author_email='boris.ivan.babic@gmail.com',
-    description='Loads cookies from your browser into a cookiejar object so can download with urllib and other libraries the same content you see in the web browser.',
-    url='https://github.com/borisbabic/browser_cookie3',
+    name='cookie-cutter',
+    version='0.2.0',
+    packages=['cookie-cutter'],
+    package_dir={'cookie-cutter' : '.'}, # look for package contents in current directory
+    author='Tyler Krupicka and Jimmy Ly',
+    author_email='site@tylerkrupicka.com',
+    description='Retrieves cookies from Chrome and Firefox and returns them as a cookie jar.',
+    url='https://github.com/tylerkrupicka/cookie-cutter',
     install_requires=['pyaes','pbkdf2','keyring'],
     license='lgpl'
 )
